51b44b92729c0ca714eb1e93b867a896b7e75e06
TapusiDaniel <daniel.tapusi@stud.acs.upb.ro>
2025-05-15 00:38:10 +0300

    Update

--- data/sections/dex16_sections.py
+++ data/sections/dex16_sections.py
@@ -39,14 +39,13 @@ DEx16_SECTIONS = {
         }
     },
     "Arad - Oradea (lot 1)": {
-        "status": "tendered",
-        "tender_end_date": "2025",
+        "status": "in_construction",
+        "progress": "0%",
+        "completion_date": "2027",
         "length": "33.7 km",
-        "estimated_cost": "548M",
-        "construction_duration": "24 de luni",
+        "constructor": "Asocierea PRECON TRANSILVANIA SRL - AUTOMAGISTRAL PIVDEN SRL",
+        "estimated_cost": "455M",
         "financing": "Fondul European de Dezvoltare Regională (60%) și bugetul de stat (40%) - Programul Transport 2021-2027",
-        "seap_code": "CN1065535",
-        "current_stage": "Semnare contract - Asocierea PRECON TRANSILVANIA SRL - AUTOMAGISTRAL PIVDEN SRL",
         "xml_file": "DEx16/dex16_arad_oradea_lot1.xml",
         "start_point": [46.793249, 21.694058],  # Salonta
         "end_point": [47.072718, 21.873508]  # Oradea
