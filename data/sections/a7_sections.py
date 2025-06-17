ab455ea71b8cebe545047b20faeb7fe61ff10121
TapusiDaniel <daniel.tapusi@stud.acs.upb.ro>
2025-06-17 17:23:46 +0300

    Update

--- data/sections/a7_sections.py
+++ data/sections/a7_sections.py
@@ -37,7 +37,7 @@ A7_SECTIONS = {
     },
     "Ploiești - Buzău (lot 3)": {
         "status": "in_construction",
-        "progress": "48%",
+        "progress": "60%",
         "completion_date": "2025",
         "length": "13.90 km",
         "constructor": "Asocierea Nurol Inşaat Ve Ticaret A.S. (lider) - Makyol Insaat Sanayi Turizm Ve Ticaret A.S.",
